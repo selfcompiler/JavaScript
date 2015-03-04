6635bba538e4b974e2bab0054c73fec041c298f0
Rahul Kumar Singh <kumarahul120292@gmail.com>
2015-03-05 01:38:07 +0530

    Push script in Head Tag .
    Push script in Head Tag .

---
+++ script_in_head.js
@@ -0,0 +1,20 @@
+<html>
+  <head>
+    <script>
+      function count() {
+        for(var i=1; i<=3; i++) {
+          alert("Rahul "+i);
+        }
+      }
+    </script>
+  </head>
+
+  <body>
+  
+  <h2>Press the button to start</h2>  
+
+    <input type="button" onclick="count()" value="Count"/>
+
+  </body>
+
+</html>
